9351c0b08741678164df354660c8654f1cc46d0e
Alex12012000 <arcex.29@gmail.com>
2022-07-05 17:44:15 +0200

    Bonus

---
+++ src/components/MyHero.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="jumbo">
+        <img src="../assets/catsnghosts.png" alt="">
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: "MyHero",
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+
+    img {
+        width: 100%;
+    }
+    .jumbo {
+        width: 100%;
+    }
+
+</style>
---
+++ src/components/MyTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="heading">
+        <h2>Il mio Titolo</h2>
+    </div>
+</template>
+
+
+<script>
+
+export default {
+    name: "MyTitle",
+}
+
+</script>
+
+
+<style lang="scss" scoped>
+
+
+
+</style>
